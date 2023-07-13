8fff100ecc3df9c70ecf394f54799e3b1fb1e594
WARNEVERCHANGE <ceckinkonstantin@gmail.com>
2023-07-13 19:12:52 +0500

    up liquibase

--- src/main/java/com/evilcorp/BankApplication.java
+++ src/main/java/com/evilcorp/BankApplication.java
@@ -1,5 +1,6 @@
 package com.evilcorp;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -11,6 +12,6 @@ public class BankApplication
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(BankApplication.class, args);
     }
 }
