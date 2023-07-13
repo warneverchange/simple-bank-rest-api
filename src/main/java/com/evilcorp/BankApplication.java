a81a9fd347fbbf92fed569fa77d6cb5b4e4f3878
WARNEVERCHANGE <ceckinkonstantin@gmail.com>
2023-07-13 16:25:33 +0500

    init

---
+++ src/main/java/com/evilcorp/BankApplication.java
@@ -0,0 +1,16 @@
+package com.evilcorp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class BankApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
